2a405c17d9bf684298b62ab8c8c5ea39eb25eee7
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-03-05 16:27:29 +0100

    jsx

---
+++ Codecademy/ReactIntroduction/JSX/IntroToJDX/06_JSXOuterElements.js
@@ -0,0 +1,26 @@
+// this will work
+const paragraphs = (
+    <div id="i-am-the-outermost-element">
+      <p>I am a paragraph.</p>
+      <p>I, too, am a paragraph.</p>
+    </div>
+);
+
+// but this will not work
+const paragraphs2 = (
+    <p>I am a paragraph.</p> 
+    <p>I, too, am a paragraph.</p>
+);
+
+const blog = (
+    <div> 
+      <img src="pics/192940u73.jpg" />
+      <h1>
+        Welcome to Dan's Blog!
+      </h1>
+      <article>
+        Wow I had the tastiest sandwich today. I <strong>literally</strong> almost freaked out.
+      </article>
+    </div>
+  );
+     
