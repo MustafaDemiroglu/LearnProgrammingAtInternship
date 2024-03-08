683fc06f0fc0927bb94a80a7c322da616746aad6
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-03-08 09:21:39 +0100

    teest

---
+++ Codecademy/ReactIntroduction/ReactComponents/1_YourFirstReactComponent/6_ReturnKeyword.js
@@ -0,0 +1,15 @@
+function Button(){
+    // Instructions go here, between the curly braces.
+} 
+
+function BackButton() {
+    return <button>Back To Home</button>;
+}
+
+
+// Example App.js
+import React from 'react';
+
+function MyComponent() {
+  return <h1>Hello World</h1>;
+}
