0b0eec89107c889c2336efc1b03de071ac493be9
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-03-11 15:31:19 +0100

    project finished

--- Codecademy/ReactIntroduction/projectcodeyoverflowforum/src/App.js
+++ Codecademy/ReactIntroduction/projectcodeyoverflowforum/src/App.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import {comments} from './commentData'
+import Card from './Card';
+
+function App(){
+
+ return comments.map(comment =>
+  <Card commentobject = {comment}/>
+);
+}
+export default App;
--- Codecademy/ReactIntroduction/projectcodeyoverflowforum/src/Header.js
+++ Codecademy/ReactIntroduction/projectcodeyoverflowforum/src/Header.js
@@ -1,2 +1,12 @@
 import React from 'react';
 
+function Header (props) {
+  return (
+    <div>
+    <img src={props.profileImg}/>
+    <h1>{props.userName}</h1>
+    </div>
+  );
+}
+
+export default Header;
