4c3de618a1f4c858007755c288beb9fa86d006ca
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-03-19 16:29:29 +0100

    code challenge

---
+++ Codecademy/Front-End Engineer Career Path/Javascript Part II/Code Challenges Intemediate Js/12_sortSpeciesByTeeth.js
@@ -0,0 +1,14 @@
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+
+// Write your code here:
+const sortSpeciesByTeeth = arr => arr.sort((obj1, obj2) => obj1.numTeeth>obj2.numTeeth)
+
+// Feel free to comment out the code below when you're ready to test your function!
+
+console.log(sortSpeciesByTeeth(speciesArray))
+
+// Should print:
+// [ { speciesName: 'human', numTeeth: 32 },
+//   { speciesName: 'dog', numTeeth: 42 },
+//   { speciesName: 'shark', numTeeth: 50 },
+//   { speciesName: 'alligator', numTeeth: 80 } ]
