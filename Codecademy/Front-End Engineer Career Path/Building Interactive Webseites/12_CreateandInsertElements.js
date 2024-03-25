5e6b38d2be4d6c7431c7c6466c8677ba976f65e6
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-03-25 07:44:26 +0100

    traversimg

---
+++ Codecademy/Front-End Engineer Career Path/Building Interactive Webseites/12_CreateandInsertElements.js
@@ -0,0 +1,11 @@
+let paragraph = document.createElement('p');
+paragraph.id = 'info'; 
+paragraph.innerHTML = 'The text inside the paragraph';
+paragraph.id = 'info'; 
+paragraph.innerHTML = 'The text inside the paragraph';
+
+
+let newAttraction = document.createElement('li');
+newAttraction.id = 'vespa';
+newAttraction.innerHTML = 'Rent a Vespa';
+document.getElementById('italy-attractions').appendChild(newAttraction);
