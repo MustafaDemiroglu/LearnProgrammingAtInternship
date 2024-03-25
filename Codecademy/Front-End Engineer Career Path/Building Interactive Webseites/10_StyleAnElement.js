8b00ef4654e11fee3db4e335e4b699c63a46a84d
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-03-25 07:12:56 +0100

    style

---
+++ Codecademy/Front-End Engineer Career Path/Building Interactive Webseites/10_StyleAnElement.js
@@ -0,0 +1,2 @@
+document.querySelector('body').style.backgroundColor = '#201f2e';
+document.querySelector('.heading').style.fontFamily = 'Roboto';
---
+++ Codecademy/Front-End Engineer Career Path/Building Interactive Webseites/9_SelectAndModify.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName('ranking')[0].innerHTML = 'Most popular Harry Potter characters'
+document.getElementById('fourth').innerHTML = 'Professor Snape'
+document.getElementsByClassName('slytherin')[0].innerHTML = 'Salazar Slytherin'
+document.getElementsByTagName('li')[0].innerHTML = 'Dobby'
